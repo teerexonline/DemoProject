073169dda15ab434b28a926cc8871bde96af74ac
Taiwo Amida <teerex@taiwos-mbp.home>
2026-04-05 01:44:17 -0400

    Performance improvement

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: [
+      '@supabase/supabase-js',
+      '@supabase/ssr',
+    ],
+  },
+}
 
 export default nextConfig
